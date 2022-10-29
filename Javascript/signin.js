a483f75f9be791a3e0eb6cd926e0d69fb7cab2e5
Prshnt Bhushal <prshnt.bhushal@gmail.com>
2022-10-29 19:01:29 +0545

    sign up page done

--- Javascript/signin.js
+++ Javascript/signin.js
@@ -1,38 +1,50 @@
-// import { createUserWithEmailAndPassword } from "firebase/auth";
-// import { auth } from "../firebaselink";
-// import { store } from "../firebaselink";
-// import { doc, setDoc } from "firebase/firestore";
-const btn_register =document.querySelector('#btn-register');
-btn_register.addEventListener('click', ()=>{
-  console.log("heeeeee")
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import {getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+import { getFirestore } from  "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkhqLVV1-YjUWLOx76M0zZ7ZYfBQFyglk",
+  authDomain: "trade-exchange-2022.firebaseapp.com",
+  projectId: "trade-exchange-2022",
+  storageBucket: "trade-exchange-2022.appspot.com",
+  messagingSenderId: "291602088991",
+  appId: "1:291602088991:web:544e47f4008e4bf2de412a",
+  measurementId: "G-BXNN9K3MWP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const store = getFirestore(app);
+
+const btn_register =document.getElementById('btn-register');
+btn_register.addEventListener('click',()=>{
+  console.log("hello")
+    const email =document.getElementById('sign-up-email').value;
+    const pass =document.getElementById('sign-up-password').value;
+    const username =document.getElementById('sign-up-username').value;
+    createUserWithEmailAndPassword(auth, email, pass)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    const user_id= userCredential.user.uid;
+    const data ={
+        email: email,
+        username: username,
+        password:pass
+    }
+    const docRef= doc(store,"user",user_id)
+    setDoc(docRef,data).then((documentRef)=>{
+        alert("user has been registered")
+        window.location.href = "/Html/Market.html";
+    })
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(error.message)
+  });
 })
-// function speed(){
-//   console.log("heloo")
-// }
-// btn_register.addEventListener('click',()=>{
-//   console.log("hello")
-//     const email =document.querySelector('#sign-up-email');
-//     const pass =document.querySelector('#sign-up-password');
-//     const username =document.querySelector('#sign-up-username');
-//     createUserWithEmailAndPassword(auth, email, pass)
-//   .then((userCredential) => {
-//     // Signed in 
-//     const user = userCredential.user;
-//     const user_id= userCredential.user.uid;
-//     const data ={
-//         email: email,
-//         username: username,
-//         password:pass
-//     }
-//     const docRef= doc(store,"user",user_id)
-//     setDoc(docRef,data).then((documentRef)=>{
-//         alert("user has been registered")
-//     })
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     alert(error.message)
-//   });
-// })
+export default app;
