fd80853cc9c12ae4ff8fbc83a8e3bb6131c26cea
Prshnt Bhushal <prshnt.bhushal@gmail.com>
2022-10-30 08:15:26 +0545

    sigin part done

--- Javascript/signin.js
+++ Javascript/signin.js
@@ -1,6 +1,6 @@
 import { doc, setDoc } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js"
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
-import {getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+import {getAuth, createUserWithEmailAndPassword ,signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
 import { getFirestore } from  "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -17,7 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const store = getFirestore(app);
-
+//sign Up
 const btn_register =document.getElementById('btn-register');
 btn_register.addEventListener('click',()=>{
   console.log("hello")
@@ -36,8 +36,8 @@ btn_register.addEventListener('click',()=>{
     }
     const docRef= doc(store,"user",user_id)
     setDoc(docRef,data).then((documentRef)=>{
-        alert("user has been registered")
-        window.location.href = "/Html/Market.html";
+        alert("user has been registered proceed to login")
+        window.location.href = "/Html/signup-signin.html";
     })
     // ...
   })
@@ -47,4 +47,27 @@ btn_register.addEventListener('click',()=>{
     alert(error.message)
   });
 })
+
+// sign in
+
+const btn_login =document.getElementById('btn-login');
+btn_login.addEventListener('click',()=>{
+  console.log("hello")
+    const email$ =document.getElementById('sign-in-email').value;
+    const pass$ =document.getElementById('sign-in-password').value;
+    signInWithEmailAndPassword(auth, email$, pass$)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    alert("welcome To TradeEx")
+    window.location.href = "/Html/Market.html";
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(error.message)
+  });
+})
+
 export default app;
