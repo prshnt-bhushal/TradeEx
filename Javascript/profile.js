9b6fad99008948397ef456ba8c454fcddb2b83df
Prshnt Bhushal <prshnt.bhushal@gmail.com>
2022-10-31 14:01:56 +0545

    post uploaded

--- Javascript/profile.js
+++ Javascript/profile.js
@@ -1,4 +1,4 @@
-import { collection, addDoc } from  "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
+import { collection, addDoc, getDoc, onSnapshot, query, where } from  "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
 import { ref, uploadBytes, getDownloadURL } from  "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js";
 import { returnStore } from './firebasefile.js';
 import { returnStore_age } from './firebasefile.js';
@@ -8,17 +8,23 @@ import { returnAuth } from './firebasefile.js';
 const auth =returnAuth();
 const store_age =returnStore_age();
 const store =returnStore();
+const user_name =auth.currentUser.displayName;
+// let username_display =document.getElementById('usernameDisplay').innerText;
+// username_display.innerText=user_name;
 const btn_upload =document.getElementById("btn-productUpload");
 btn_upload.addEventListener('click',()=>{
     console.log('helloo my ');
-    const product_name =document.getElementById('product-title');
-    const product_brand =document.getElementById('product-shortDes');
-    const product_details =document.getElementById('product-detailDes');
+    const product_name =document.getElementById('product-title').value;
+    const product_brand =document.getElementById('product-shortDes').value;
+    const product_details =document.getElementById('product-detailDes').value;
     const product_category =document.getElementById('select-category');
     const category_selected = product_category.options[product_category.selectedIndex].text;
     const selectedFile = document.getElementById('productImg').files[0];
+    const offerprice_provided=document.getElementById('offerprice-provided').value;
+    const actualprice_provided=document.getElementById('actualprice-provided').value;
     
-    const user_id =auth.currentUser.displayName;
+    
+    const user_id =auth.currentUser.uid;
 
     const imgRef =ref(store_age,`images/${selectedFile.name}`);
     uploadBytes(imgRef, selectedFile).then(()=>{
@@ -30,9 +36,11 @@ btn_upload.addEventListener('click',()=>{
                 description:product_details,
                 category:category_selected,
                 imageUrl:imgUrl,
+                offerPrice:offerprice_provided,
+                actualPrice:actualprice_provided,
                 userId:user_id
             } 
-            const docRef = collection(store,'posts')
+            const docRef = collection(store,'post')
             addDoc(docRef,dataObject).then((userRef)=>{
                 alert('post has been uploaded tadaa')          
             })
@@ -40,3 +48,17 @@ btn_upload.addEventListener('click',()=>{
     
     })   
 })
+
+function postCall(){
+    const postRef  = collection(store,'post');
+      const q = query(postRef, where("userId", "==", auth.currentUser.displayName));
+      onSnapshot(q,(snapshot)=>{
+        const pack = snapshot.docs.map((doc)=>({
+          id: doc.id,
+          ...doc.data(),
+        }))
+        console.log('aaraha hain rukh jao');
+        console.log(pack);
+      })
+}
+postCall();
